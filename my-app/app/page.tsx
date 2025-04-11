28d5744093ead68c4fbc23497e9c5d515893dca6
ganzabruce <161241733+ganzabruce@users.noreply.github.com>
2025-04-11 14:54:00 +0200

    reviewed shoun's course

--- my-app/app/page.tsx
+++ my-app/app/page.tsx
@@ -1,12 +1,11 @@
 import Image from "next/image";
-import ProductCard from "./components/productCard";
+
 
 export default function Home() {
   return (
     <main>
       <h1>
-        hello world
-        <ProductCard />
+        <h1>hello world</h1>
       </h1>
     </main>
   );
